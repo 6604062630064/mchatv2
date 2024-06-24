6a63f7172f4f9dd730f4f9033fdf8a4485261360
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2024-06-24 23:41:48 +0700

    Some adjustments

--- backend/src/routes/UsersRouter.ts
+++ backend/src/routes/UsersRouter.ts
@@ -1,15 +1,15 @@
 import { Router, Express, Request, Response } from "express";
 const router = Router();
 
-router.get("/:Userid", (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response) => {
 	res.send("To be implemented");
 });
 
-router.get("/:Userid/messages", (req: Request, res: Response) => {
+router.get("/:userid/messages", (req: Request, res: Response) => {
 	res.send("To be implemented");
 });
 
-router.post("/:Userid/messages", (req: Request, res: Response) => {
+router.post("/:userid/messages", (req: Request, res: Response) => {
 	res.send("To be implemented");
 });
 
