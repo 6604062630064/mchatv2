41e34aabc34a5af6d179570b4033bb4a801754c5
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2024-06-24 23:32:52 +0700

    Setup auth routes

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,10 +1,18 @@
+import "module-alias/register";
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const morgan = require("morgan");
+const cors = require("cors");
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const authRouter = require("@/routes/authRouter");
+
+app.use(cors());
+app.use(morgan("combined"));
+app.use("/auth", authRouter);
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Express + TypeScript Server");
---
+++ backend/src/routes/authRouter.ts
@@ -0,0 +1,21 @@
+import { Router, Express, Request, Response } from "express";
+
+const router = Router();
+
+router.get("/google", (req: Request, res: Response) => {
+	res.send("To be implemented");
+});
+
+router.get("/loggedin", (req: Request, res: Response) => {
+	res.send("To be implemented");
+});
+
+router.post("/login", (req: Request, res: Response) => {
+	res.send("To be implemented");
+});
+
+router.post("/loggedin", (req: Request, res: Response) => {
+	res.send("To be implemented");
+});
+
+module.exports = router;
